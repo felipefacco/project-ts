d731f4df464f388be3b427ecb1c674431692d696
felipefacco <facco.felipe@yahoo.com>
2023-12-08 15:57:25 -0300

    edit

--- scripts/Scripts Download.R
+++ scripts/Scripts Download.R
@@ -1,7 +1,10 @@
+# para fazer o download fiz ano a ano, o link para os anos anteriores esta dentro de urls/old
+# o script ficou salvo com a formatação do ultimo ano, para baixar os interiores necessário ajustar
 
-
-filename <- dir('urls/subset_OCO2_L2_Lite_FP_11r_20230603_175134_.txt', pattern = 'teste_download')
-urls <- read.table(paste0('urls/subset_OCO2_L2_Lite_FP_11r_20230603_175134_.txt',filename))
+filename <- dir('urls/', 
+                pattern = '.txt')
+urls <- read.table(paste0('urls/', 
+                          filename))
 n_urls <- nrow(urls)
 n_split <- length(stringr::str_split(urls[1,1],"/",simplify = TRUE))
 
@@ -12,7 +15,7 @@ files_nc <- stringr::str_split_fixed(urls[,1],"/",n=Inf)[,n_split]
 for (i in 1:n_urls){
   repeat{
     dw <- try(download.file(urls[i,1],
-        paste0('arquivos/OCO2_2022/',files_nc[i]),
+        paste0('arquivos/OCO2_2022/',files_nc[i]), # ajustar pasta para diferentes anos
         method = 'wget',
         extra = c('--user=facco.felipe --password=')
                             
